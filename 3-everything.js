3b605b471704762773f52445569349b537e48ecd
irisv19 <143832578+irisv19@users.noreply.github.com>
2024-02-19 19:01:51 -0800

    done

--- 3-everything.js
+++ 3-everything.js
@@ -1,5 +1,12 @@
-function every(array, test) {
-    // Your code here.
+function every(array, predicate) {
+    for (let element of array) {
+      if(!predicate(element)) return false;
+    }
+    return true;
+  }
+
+  function every2(array, predicate) {
+    return !array.some(element => !predicate(element));
   }
   
   console.log(every([1, 3, 5], n => n < 10));
